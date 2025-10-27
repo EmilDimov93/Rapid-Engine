472b65da877c34c4a5dc40aaa02e4fb1707e655b
EmilDimov93 <emildimovjr@gmail.com>
2025-10-27 17:25:27 +0200

    Stop tracking build folder

--- build/Engine/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/user/Desktop/RapidEngine/Engine/raylib
-# Build directory: C:/Users/user/Desktop/RapidEngine/build/Engine/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
